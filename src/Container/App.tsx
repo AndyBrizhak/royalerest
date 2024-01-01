a9041445844b09d338af314226ccf887f3ab27b4
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-01-01 08:31:27 +0800

    install bootstrap

---
+++ src/Container/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import  "bootstrap/dist/js/bootstrap.js";
+import  "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <div className="text-danger">App</div>
+  );
+}
+
+export default App;
