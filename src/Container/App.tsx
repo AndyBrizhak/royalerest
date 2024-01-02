c9285a16f12200fdf33b7616b9613024668509f4
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-01-02 09:09:32 +0800

    change directory and and create components Header and Footer

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Footer, Header } from "../Components/Layout";
 import "bootstrap/dist/css/bootstrap.css";
 import  "bootstrap/dist/js/bootstrap.js";
 import  "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
-    <div className="text-danger">App</div>
+    <div>
+      <Header />
+      Main Component
+      <Footer />
+    </div>
   );
 }
 
