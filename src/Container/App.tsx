9348d9f4a8b17778e9e5b34fabb990c2be137ad5
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-01-03 08:40:23 +0800

    Header UI

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Footer, Header } from "../Components/Layout";
-import "bootstrap/dist/css/bootstrap.css";
-import  "bootstrap/dist/js/bootstrap.js";
-import  "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
