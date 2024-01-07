a7a466e39e0af7246d2584eefed9dbf970037929
Andrey Brizhak <35806172+AndyBrizhak@users.noreply.github.com>
2024-01-07 11:30:04 +0800

    change structure project

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MenuItemList } from '../Components/Layout/Page/MenuItems'
+
+function Home() {
+  return (
+    <div><div className='cotainer p-2'>
+        <MenuItemList/></div></div>
+  )
+}
+
+export default Home
